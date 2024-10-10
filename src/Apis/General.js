81a3e08dc1db5a57ba133ec583e3707a6f3c1d83
moeeztarik <90174007+moeeztarik@users.noreply.github.com>
2024-10-10 20:55:54 +0500

    update

--- src/Apis/General.js
+++ src/Apis/General.js
@@ -2,7 +2,7 @@ import api from "./Axios";
 
 export const getCities = async (dto) => {
   try {
-    const response = await api.get("general/city?limit=10&offset=0");
+    const response = await api.get("general/city?limit=30&offset=0");
     return response.data;
   } catch (error) {
     // Check if the error has a response property
